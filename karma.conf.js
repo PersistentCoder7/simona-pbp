4f25f4237ae840ef98c95967022933c02b8e43e2
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2021-03-18 21:46:04 +0530

    try chromium browser

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['chromium-browser'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
